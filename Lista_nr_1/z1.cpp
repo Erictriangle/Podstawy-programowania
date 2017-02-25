9088c0ecaa655d47475ec63088e65732c76ec2e9
EricTriangle <eric.triangle@gmail.com>
2017-02-25 12:09:14 +0000

    Zadanie2

--- Lista_nr_1/z1.cpp
+++ Lista_nr_1/z1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 using namespace std;
-int pobierz();
+void pobierz();
 int wyznaczMax(int a, int b, int c);
 int a, b, c;
 
 int main(){
-	a = pobierz();
+	pobierz();
 	cout << "Najwieksza wartosc ma zmienna:" << wyznaczMax(a, b, c) << endl;
 	return 0;
 }
 
-int pobierz(){
+void pobierz(){
 	cout << "Podaj wartosc a:" << endl;
 	cin >> a;
 	cout << "Podaj wartosc b:" << endl;
 	cin >> b;
 	cout << "Podaj wartosc c:" << endl;
 	cin >> c;
-	return a;
 }
 
 int wyznaczMax(int a, int b, int c){
---
+++ Lista_nr_1/z2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void pobierz();
+float oblicz(float a, float b);
+
+float a, b, x;
+
+int main(){
+	pobierz();
+	cout << oblicz(a, b) << endl;
+	return 0;
+}
+
+void pobierz(){
+	cout << "Podaj parametr a: " << endl;
+	cin >> a;
+	cout << "Podaj parametr b: " << endl;
+	cin >> b;
+}
+
+float oblicz(float a, float b){
+	return (-b/a);
+}
