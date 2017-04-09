0b9d762e78464889fffc6f4b2cde171902a2ec60
Eric Triangle <eric.triangle@gmail.com>
2017-04-09 12:11:17 +0000

    Zestaw 5

--- Lista_nr_5/zestaw5.cpp
+++ Lista_nr_5/zestaw5.cpp
@@ -46,15 +46,49 @@ string* add(string* &tab,string text){
 	for(unsigned int i=1; i<size+1; i++){
 		nTab[i] = tab[i];
 	}
-	nTab[size+2] = text;
+	nTab[size+1] = text;
 	
+	delete[] tab;
 	return nTab;
+}
+
+string* remove(string* tab){
+	unsigned int size =checkSize(tab);
+	string* nTab = new string[size];
+	
+	nTab[0] = to_string(size-1);
+	for(unsigned int i=1; i<size; i++){
+		nTab[i] = tab[i];
+	}
+	
+	delete[] tab;
+	return nTab;
+}
+
+void find(string* &tab,string text){
+		unsigned int size = checkSize(tab);
+		string spr = "Nie znaleziono!";
+		
+		for(unsigned int i=1; i<size+1; i++){
+			if(!text.compare(tab[i])) spr = tab[i];
+		}
+		cout << spr << endl;
+}
+
+void save(fstream& plik,string* &tab){
+	unsigned int size =checkSize(tab);
+	
+	plik << size << endl;
+	for(unsigned int i=1; i<size+1; i++){
+		plik << tab[i] << endl;
+	}
 }
 
 int main(){
 	char chose = 'e';
-	fstream iPlik("iPlikZ5s.txt");
-	string* tab, tab1;
+	fstream iPlik("iPlikZ5.txt");
+	fstream oPlik("iPlikZ5.txt");
+	string *tab;
 	string text;
 	
 	do{
@@ -63,7 +97,8 @@ int main(){
 	cout << "i - wczytaj baze danych" << endl;
 	cout << "o - wypisz zawartosc bazy danych" << endl;
     cout << "a - dodaj nowe haslo" << endl;
-	cout << "d - usun haslo" << endl;
+	cout << "d - usun ostatnie haslo" << endl;
+	cout << "f - znajdz haslo" << endl;
 	cout << "s - zapisz zmiany" << endl;
 	cout << "e - opusc program" << endl;	
 	cin >> chose;
@@ -71,7 +106,7 @@ int main(){
 	switch(chose){
 		case 'i':
 			if(!iPlik){
-				cerr << "Blad wczytania pliku!" << endl;
+				cerr << "Bląd wczytania pliku!" << endl;
 				return -1;
 			}
 			tab = load(iPlik);
@@ -84,7 +119,24 @@ int main(){
 		case 'a':
 				cout << "Wpisz nowe haslo:" << endl;
 				cin >> text;
-				tab1 = add(tab,text);
+				tab = add(tab,text);
+			break;
+		
+		case 'd':
+				tab = remove(tab);
+			break;
+		
+		case 'f':
+			cout << "Wpisz szukane haslo:" << endl;
+			cin >> text;
+			find(tab,text);
+			break;
+		
+		case 's':
+			if(!oPlik){
+				cerr << "Błąd wczytania pliku!" << endl;
+			}
+			save(oPlik,tab);
 			break;
 		
 		case 'e':
@@ -97,5 +149,6 @@ int main(){
 	}
 	} while(chose != 'e');
 
+	delete[] tab;
 	return 0;
 }
